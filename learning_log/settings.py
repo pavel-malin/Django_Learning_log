dbfa272083f39d3d7059d34a29d129bd8fe6189a
Olga <davidovich.olga.v@mail.ru>
2019-09-21 12:54:47 +0300

    Set DEBUG=False for Heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -126,7 +126,9 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # Разрешены все заголовки хостов
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['learning-log-for-you.herokuapp.com']
+
+    DEBUG = False
 
     # Конфигурация статических ресуров
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
